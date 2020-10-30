f4a64277c35b563ef2368ff88ce80d2e6c52e113
Dana Ciocan <danaciocan@gmail.com>
2020-10-30 21:08:33 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Layout from "../components/Layout"
+
+const Index = ({ title, description, ...props }) => {
+  return <Layout pageTitle={title}>
+    <h1 className="title">Welcome to my blog!</h1>
+    <p className="description">{description}</p>
+    <main>
+      Posts go here!
+    </main>
+  </Layout>
+}
+
+export default Index
+
+export async function getStaticProps() {
+  const configData = await import(`../siteconfig.json`)
+
+  return {
+    props: {
+      title: configData.default.title,
+      description: configData.default.description,
+    },
+  }
+}
